476b64a3c26fdf7674285cb3f55c5292557d7115
RoJoSaVs <43160386+RoJoSaVs@users.noreply.github.com>
2023-05-11 18:52:16 -0600

    app ready

--- mobileapp/app/src/main/java/com/example/mobile_app/MainActivity.kt
+++ mobileapp/app/src/main/java/com/example/mobile_app/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.mobile_app
 
-import android.app.Activity
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absolutePadding
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,25 +17,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Surface
@@ -52,28 +34,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.toSize
 import com.example.mobile_app.ui.theme.MobileappTheme
 
 class MainActivity : ComponentActivity() {
@@ -86,8 +56,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    getCode()
-                    numPath(1.0f)
+                    getCode()
+//                    numPath(1.0f)
                 }
             }
         }
@@ -96,7 +66,7 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun numPath(sizeNumPad:Float)
+fun numPath(sizeNumPad:Float, userInputFromTop:String)
 {   var textInputFromPad by remember { mutableStateOf("") }
     val mContext = LocalContext.current
 
@@ -106,6 +76,9 @@ fun numPath(sizeNumPad:Float)
     val mMediaPlayer2 = MediaPlayer.create(mContext, R.raw.click2)
     val level = MediaPlayer.create(mContext, R.raw.level)
     val damage = MediaPlayer.create(mContext, R.raw.damage)
+
+    // Restart the app
+    var displayMainPage by remember {mutableStateOf(false)}
 
     Box(modifier = Modifier.background(Color.DarkGray)) {
         Column(
@@ -161,7 +134,7 @@ fun numPath(sizeNumPad:Float)
 
             Button(
                 onClick = {
-                    if(textInputFromPad == "1234"){
+                    if(textInputFromPad == userInputFromTop){
                         openDialog.value = true
                         level.start()
                     }
@@ -197,6 +170,7 @@ fun numPath(sizeNumPad:Float)
                             onClick = {
                                 openDialog.value = false
                                 // do something else
+                                displayMainPage = true
                             }) {
                             Text("Done")
                         }
@@ -205,6 +179,9 @@ fun numPath(sizeNumPad:Float)
             }
         }
     }
+    if (displayMainPage){
+        getCode()
+    }
 }
 
 
@@ -212,83 +189,87 @@ fun numPath(sizeNumPad:Float)
 
 
 
+//@OptIn(ExperimentalMaterial3Api::class)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun getCode()
 {
+    var displayNumpad by remember {mutableStateOf(false)}
     var codeUserInputValue by remember { mutableStateOf(TextFieldValue("")) }
     var sliderPosition by remember { mutableStateOf(1f) }
-
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .absolutePadding(0.dp, 100.dp, 0.dp, 0.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "Set attributes",
-            color = Color.Blue,
-            fontSize = 30.sp,
-            fontFamily = FontFamily.Monospace,
-            textAlign = TextAlign.Center,
-        )
-    }
-
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .absolutePadding(10.dp, 200.dp, 10.dp, 0.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        TextField(
-            value = codeUserInputValue,
-            label = {Text(text = "Add your code")},
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            onValueChange = {
-                if(it.text.length <= 4 ) {
-                    codeUserInputValue = it
-                }
-            }
-        )
-    }
-
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .absolutePadding(30.dp, 300.dp, 30.dp, 0.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "Select number size",
-            color = Color.Blue,
-            fontSize = 20.sp,
-            fontFamily = FontFamily.Monospace,
-            textAlign = TextAlign.Center,
-        )
-
-
-        Text(text = sliderPosition.toString(), style = MaterialTheme.typography.labelLarge)
-        Slider(
-            value = sliderPosition,
-            onValueChange = { sliderPosition = it },
-            valueRange = 1f..3f,
-            steps = 1,
-            colors = SliderDefaults.colors(
-                thumbColor = MaterialTheme.colorScheme.secondary,
-                activeTrackColor = MaterialTheme.colorScheme.secondary
+    Box(modifier = Modifier.background(Color.DarkGray)) {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .absolutePadding(0.dp, 100.dp, 0.dp, 0.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = "Set attributes",
+                color = Color.White,
+                fontSize = 30.sp,
+                fontFamily = FontFamily.Monospace,
+                textAlign = TextAlign.Center,
             )
-        )
-    }
+        }
 
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .absolutePadding(10.dp, 600.dp, 10.dp, 0.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    )
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .absolutePadding(10.dp, 200.dp, 10.dp, 0.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            TextField(
+                value = codeUserInputValue,
+                label = {Text(text = "Add your code")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                onValueChange = {
+                    if(it.text.length <= 10 ) {
+                        codeUserInputValue = it
+                    }
+                }
+            )
+        }
+
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .absolutePadding(30.dp, 300.dp, 30.dp, 0.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = "Select number size",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Monospace,
+                textAlign = TextAlign.Center,
+            )
+
+
+            Text(text = sliderPosition.toString(), style = MaterialTheme.typography.labelLarge)
+            Slider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                valueRange = 1f..3f,
+                steps = 1,
+                colors = SliderDefaults.colors(
+                    thumbColor = MaterialTheme.colorScheme.secondary,
+                    activeTrackColor = MaterialTheme.colorScheme.secondary
+                )
+            )
+        }
+
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .absolutePadding(10.dp, 600.dp, 10.dp, 0.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
         {
-            Button(onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(Color(0XFF0F9D58)),
+            Button(onClick = {
+                 displayNumpad = true
+                             },
+                colors = ButtonDefaults.buttonColors(Color.Blue),
                 modifier = Modifier
                     .height(60.dp)
                     .width(100.dp)
@@ -296,4 +277,8 @@ fun getCode()
                 Text("Done", color = Color.White, fontSize = 20.sp)
             }
         }
+    }
+    if (displayNumpad){
+        numPath(sizeNumPad = sliderPosition, userInputFromTop = codeUserInputValue.text)
+    }
 }
