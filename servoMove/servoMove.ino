da670b8a94daac2137d78db6abcb8d5c98387860
Jess <jessicaes2000@gmail.com>
2023-06-05 23:20:10 -0600

    [UPDATE/ADD] arduino functional code

--- servoMove/servoMove.ino
+++ servoMove/servoMove.ino
@@ -4,7 +4,9 @@
 Servo servo1;  // Create a servo object
 Servo servo2;  // Create a servo object
 Servo servo3                                          ;  // Create a servo object
-int currentPosition;
+int currentPositionServo1 = 90;
+int currentPositionServo2 = 0;
+int currentPositionServo3 = 45;
 
 
 void setup() {
@@ -12,13 +14,13 @@ void setup() {
   servo2.attach(10);  // Connect the SG90 servo to pin 10
   servo3.attach(5);  // Connect the SG90 servo to pin 11
 
-  servo1.write(90);   // Move the servo to 0 degrees position
-  servo2.write(50);   // Move the servo to 0 degrees position
-  servo3.write(10);   // Move the servo to 0 degrees position
+  servo1.write(currentPositionServo1);   // Move the servo to 0 degrees position
+  servo2.write(currentPositionServo2);   // Move the servo to 0 degrees position
+  servo3.write(currentPositionServo3);   // Move the servo to 0 degrees position
   Serial.begin(9600);  // Initialize serial communication
 }
 
-void moveServo(int targetPosition, Servo servo){
+void moveServo(int currentPosition, int targetPosition, Servo servo){
   if (currentPosition < targetPosition) {
     for(int i = currentPosition; i < targetPosition; i++){
       servo.write(i);
@@ -33,45 +35,67 @@ void moveServo(int targetPosition, Servo servo){
   }
 }
 
+Servo getServo(int number){
+    int servo = number % 10;
+    switch (servo) {
+
+    case 1:
+    return servo1;
+      break;
+
+    case 2:
+    return servo2;
+      break;
+
+    case 3:
+    return servo3;
+      break;
+
+    default:
+      // Code to be executed if the switch state is neither HIGH nor LOW
+      break;
+  }
+}
+
+int getCurrentPosition(int number){
+    int servo = number % 10;
+    int result = 0;
+    int angleValue = number/10;
+    
+    switch (servo) {
+
+    case 1:
+      result = currentPositionServo1;
+      currentPositionServo1 = angleValue;
+      break;
+
+    case 2:
+      result = currentPositionServo2;
+      currentPositionServo2 = angleValue;
+      break;
+
+    case 3:
+      result = currentPositionServo3;
+      currentPositionServo3 = angleValue;
+      break;
+
+    default:
+      // Code to be executed if the switch state is neither HIGH nor LOW
+      break;
+  }
+  return result;
+}
 
 void loop() {
   if (Serial.available()) {
     int number = Serial.parseInt();  // Read the incoming number from serial monitor
 
+    if (!Serial.parseFloat()) {
 
-    moveServo(number, servo1);
-    // switch (number) {
+      Servo servo = getServo(number);
+      int currentPositionServo = getCurrentPosition(number);
 
-    //   case 1:
-    //     moveServo(90, 12.8, servo1);
-    //     Serial.println("Servo 1 moved to 90 degrees");
-    //     break;
-    //   case 2:
-    //     moveServo(12.8, 90, servo1);
-    //     Serial.println("Servo 1 moved to 180 degrees");
-    //     break;
-    //   case 3:
-    //     moveServo(5, 90, servo2);
-    //     Serial.println("Servo 2 moved to 0 degrees");
-    //     break;
-    //   case 4:
-    //     moveServo(90, 5, servo2);
-    //     Serial.println("Servo 2 moved to 90 degrees");
-    //     break;
-
-    //   case 5:
-    //     moveServo(150, 70, servo3);
-    //     Serial.println("Servo 3 moved to 90 degrees");
-    //     break;
-    //   case 6:
-    //     moveServo(70, 150, servo3);
-    //     Serial.println("Servo 3 moved to 180 degrees");
-    //     break;
-    //   default:
-    //     Serial.println("Invalid number");
-        
-
-    //     break;
-    // }
+      moveServo(currentPositionServo, number/10, servo);
+    }
   }
 }
--- socketApp/Library/serialCommArduino.h
+++ socketApp/Library/serialCommArduino.h
@@ -5,4 +5,8 @@
 #ifndef SOCKETAPP_SERIALCOMMARDUINO_H
 #define SOCKETAPP_SERIALCOMMARDUINO_H
 
+
+int initSerialComm();
+int sendCommandToArduino(int serialPort, const char* command);
+
 #endif //SOCKETAPP_SERIALCOMMARDUINO_H
